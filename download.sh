6bfbc5073ca39b963ca0cb9562968631d0bf4cf9
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2022-08-09 12:24:30 +0900

    update

--- download.sh
+++ download.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-. $(dirname $0)/path.sh
 
-if [ -f hphi-$VERSION_BASE.orig.tar.gz ]; then
+VERSION=$(head -1 debian/changelog | sed 's/1://g' | cut -d ' ' -f 2 | sed 's/[()]//g')
+VERSION_BASE=$(echo ${VERSION} | cut -d '-' -f 1)
+
+if [ -f hphi-${VERSION_BASE}.orig.tar.gz ]; then
   exit 127
 fi
+
 wget https://github.com/issp-center-dev/HPhi/releases/download/v${VERSION_BASE}/HPhi-${VERSION_BASE}.tar.gz
 tar zxvf HPhi-${VERSION_BASE}.tar.gz
-mv -f HPhi-$VERSION_BASE hphi-$VERSION_BASE
-tar zcvf hphi_$VERSION_BASE.orig.tar.gz hphi-$VERSION_BASE
-rm -rf hphi-$VERSION_BASE HPhi-$VERSION_BASE.tar.gz
+mv -f HPhi-${VERSION_BASE} hphi-${VERSION_BASE}
+tar zcvf hphi_${VERSION_BASE}.orig.tar.gz hphi-${VERSION_BASE}
+rm -rf hphi-${VERSION_BASE} HPhi-${VERSION_BASE}.tar.gz
